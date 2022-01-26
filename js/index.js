19c3ed20e3ec0b7c46feeeb45293a7e5bf37194b
Mohit Patel <45488208+mohit-design@users.noreply.github.com>
2022-01-26 10:49:21 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -11,4 +11,45 @@ $(document).ready(function() {
     fade: true,
     cssEase: 'linear'
   });
+  // Our Products Section Slick Slider JS
+  $(".slick-slider-products-home").slick({
+    arrows: true,
+    dots: false,
+    infinite: false,
+    speed: 1400,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 $(document).ready(function() {
-	// Scroll To Top JS
-  var webPageffset = 200;
-  var duration = 500;
-  $(window).scroll(function() {
-    if($(this).scrollTop() > webPageffset) {
-      $(".scroll-to-top").addClass("active");
-    } else {
-      $(".scroll-to-top").removeClass("active");
-    }
-  });
-  $(".scroll-to-top").click(function() {
-    $('html,body').animate({scrollTop: 0},2000);
+  // On Scroll Up Desktop Header JS
+	var lastScrollTop = 0,
+  offsetTopMenu = 0;
+	$(window).scroll(function (event) {
+		var st = $(this).scrollTop();
+		if (Math.abs(lastScrollTop - st) <= offsetTopMenu)
+			return;
+		if (st > lastScrollTop) {
+			// Down Scroll code
+			$(".bg-logo-navbar-menu-desktop").css({
+				"top":"-116px",
+				"box-shadow":"none"
+			});
+		} else {
+			// Up Scroll code
+			$(".bg-logo-navbar-menu-desktop").css({
+				"top": "0",
+				"box-shadow": "0 5px 12px 1px rgba(35, 31, 32, 0.09)"
+			});
+		}
+		lastScrollTop = st;
+	});
+	// Hamburger Menu and Navbar Menu JS
+  $(".burger-menu").click(function() {
+    $(this).toggleClass("active");
+    $("body").toggleClass("active");
+    $(".fixed-navbar-menu-mobile").toggleClass("active");
+    $(".menu-listing-mobile").toggleClass("active");
   });
 });
-// Remove URL When Refreshing Page
-window.history.replaceState(null, null, window.location.pathname);
